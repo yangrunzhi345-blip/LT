04eeeba559b4dba3f6462cd148e8c6673b6721c6
yrz <yrz@example.com>
2026-09-14 14:44:10 +0800

    feat(brand): apply logo as app icon

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -14,6 +14,22 @@ struct _MyApplication {
 
 G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
 
+// Loads the bundled icon rather than relying on a working-directory-relative
+// path, which differs between `flutter run` and installed application bundles.
+static void set_window_icon(GtkWindow* window) {
+  g_autofree gchar* executable_path =
+      g_file_read_link("/proc/self/exe", nullptr);
+  if (executable_path == nullptr) {
+    return;
+  }
+
+  g_autofree gchar* executable_directory = g_path_get_dirname(executable_path);
+  g_autofree gchar* icon_path = g_build_filename(
+      executable_directory, "data", "app_icon.png", nullptr);
+  g_autoptr(GError) error = nullptr;
+  gtk_window_set_icon_from_file(window, icon_path, &error);
+}
+
 // Called when first Flutter frame received.
 static void first_frame_cb(MyApplication* self, FlView* view) {
   gtk_widget_show(gtk_widget_get_toplevel(GTK_WIDGET(view)));
@@ -24,6 +40,7 @@ static void my_application_activate(GApplication* application) {
   MyApplication* self = MY_APPLICATION(application);
   GtkWindow* window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
+  set_window_icon(window);
 
   // Use a header bar when running in GNOME as this is the common style used
   // by applications and is the setup most users will be using (e.g. Ubuntu
